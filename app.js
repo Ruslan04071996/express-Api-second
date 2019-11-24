8d0956618bfb8af28cf7974aa055efbb9cd33dd7
Ruslan04071996 <ruslan040796@mail.ru>
2019-11-24 17:02:26 +0300

    third video : finished refactoring

--- app.js
+++ app.js
@@ -24,16 +24,12 @@ server.post('/posts', (req, res) => {
     const body = req.body;
     const id = body.id;
     if (id === 0) {
-        posts.push({
-            id: nextId++,
-            content: body.content,
-            likes: 0,
-        });
+        posts = [...posts, { id: nextId++, content: body.content, likes: 0, }]
         res.send(posts);
         return;
     }
-    posts = posts.map(o => o.id !== id ? o : {...o, content:body.content});//Использование метода спред , тернарного оператора вместо иф и если у нас только один аргумето , то опускаем return
-    
+    posts = posts.map(o => o.id !== id ? o : { ...o, content: body.content });//Использование метода спред , тернарного оператора вместо иф и если у нас только один аргумето , то опускаем return
+
     res.send(posts);
 });
 // Для удаления постов используем метод filter
@@ -48,11 +44,11 @@ server.delete('/posts/:id', (req, res) => {
 server.post('/posts/:id/likes', (req, res) => {
     const id = Number(req.params.id);
     const index = findPostIndexById(id);
-    if (index === -1) {
+    if(index === -1) {
         res.status(404).send(errorNotFound);
         return;
-    }
-    posts[index].likes++;
+    };
+    posts = posts.map(o => o.id !== id ? o : { ...o, likes: o.likes + 1 })
     res.send(posts)
 });
 
@@ -60,11 +56,11 @@ server.post('/posts/:id/likes', (req, res) => {
 server.delete('/posts/:id/likes', (req, res) => {
     const id = Number(req.params.id);
     const index = findPostIndexById(id);
-    if (index === -1) {
+    if(index === -1) {
         res.status(404).send(errorNotFound);
         return;
-    }
-    posts[index].likes--;
+    };
+    posts = posts.map(o => o.id !== id ? o : { ...o, likes: o.likes - 1 })
     res.send(posts)
 });
 server.listen(process.env.PORT || 9999);
