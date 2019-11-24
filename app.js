64e0bd6bce65ca81964377b558d8fe919fde4e0d
Ruslan04071996 <ruslan040796@mail.ru>
2019-11-24 15:28:09 +0300

    Second Video

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+function findPostIndexById(id) {
+    return posts.findIndex( o => o.id === id)
+    
+}
 server.get('/posts', (req, res) => {
     res.send(posts);
 });
@@ -26,7 +30,7 @@ server.post('/posts', (req, res) => {
         res.send(posts);
         return;
     }
-    const index = posts.findIndex(o => o.id === body.id);
+    const index = findPostIndexById(body.id);
     if (index === -1) {
         res.status(404).send(errorNotFound);
         return;
@@ -38,12 +42,36 @@ server.post('/posts', (req, res) => {
 
 server.delete('/posts/:id', (req, res) => {
     const id = Number(req.params.id);
-const index = posts.findIndex( o => o.id === id)
+    const index = findPostIndexById(id);
     if (index === -1) {
         res.status(404).send(errorNotFound);
         return;
     }
     posts.splice(index , 1);
     res.send(posts);
+});
+
+// Лайки отправляются на сервер
+server.post('/posts/:id/likes' , (req, res) => {
+    const id = Number(req.params.id);
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+    posts[index].likes++;
+    res.send(posts)
+});
+
+// Убирает количество лайков с нашего сервера
+server.delete('/posts/:id/likes' , (req, res) => {
+    const id = Number(req.params.id);
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+    posts[index].likes--;
+    res.send(posts)
 });
 server.listen(process.env.PORT || 9999);
